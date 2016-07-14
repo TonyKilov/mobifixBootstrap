ee76e5fb3632918fea9766076cfe83bd83de33a8
TonyKilov <testerhinoda@gmail.com>
2016-07-14 10:55:19 +0600

    Modification footer

--- js/accordion.js
+++ js/accordion.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
-            // Setup HoverAccordion for Example 2 with some custom options
-            $('.accordion').hoverAccordion({
-                activateitem: '1',
-                speed: 'fast'
+            $(".accordion  ul").hoverAccordion({
+                activateitem: "1",
+                speed: "fast"
             });
-            $('.accordion').children('li:first').addClass('firstitem');
-            $('.accordion').children('li:last').addClass('lastitem');
+            $(".accordion").children("li:first").addClass("firstitem");
+            $(".accordion").children("li:last").addClass("lastitem");
 });
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+    navigation: true,
+    navigationText: ["<span class='glyphicon glyphicon-menu-left'></span>","<span class='glyphicon glyphicon-menu-right'></span>"]
+  });
+
+  $("#owl-demo-1").owlCarousel({
+    navigation: true,
+    navigationText: ["<span class='glyphicon glyphicon-menu-left'></span>","<span class='glyphicon glyphicon-menu-right'></span>"]
+  });
+ 
+});
+
+/* */
